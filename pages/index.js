61085f4ee6639fa7114d408b42b21f3ffb600944
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-15 11:35:01 +0200

    refactor/mockdata

--- pages/index.js
+++ pages/index.js
@@ -43,14 +43,14 @@ function HomePage({ productsData, pagesCount, page }) {
                   </div>
                   <a className='flex items-center justify-center my-auto  w-auto overflow-hidden'>
                     <Image src={`/slideshow1.png`} alt="" height="300" width="200" style={{ maxHeight: "300px" }} />
-                    {/* <Image src={product.image} alt="" height="300" width="200" style={{ maxHeight: "300px" }} /> */}
+                    {/* <Image alt={product.name} src={product.image}  height="300" width="200" style={{ maxHeight: "300px" }} /> */}
                   </a>
                   <div className='flex justify-center flex-col px-3 h-40'>
-                    <h3 className='text-lg text-normal-gray font-bold h-8'>{product.name}  </h3>
+                    <h3 className='text-lg text-normal-gray font-bold h-8 truncate'>{product.name}  </h3>
                     <p>{product.category}</p>
                     <div className='price flex items-center '>
-                      <h4 className='before-discountfont-bold text-lg text-black mr-3' >{product.price - product.price * (product.discount / 100)} $</h4>
-                      <h4 className='after-discount font-medium  text-md line-through text-slate-500'>{product.price} $</h4>
+                      <h4 className='before-discountfont-bold text-lg text-black mr-3' >{(product.price - product.price * (product.discount / 100)).toFixed(2)} $</h4>
+                      <h4 className='after-discount font-medium  text-md line-through text-slate-500'>{product.price.toFixed(2)} $</h4>
                     </div>
                     <div className='h-5 w-5 my-1'><FontAwesomeIcon icon={faStar} /></div>
                     <button
